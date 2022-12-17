77318dda57fac185ca85b9c2e941211a71a3d296
mrx-master <fakeufcid@gmail.com>
2022-12-17 18:09:23 +0000

    Task 2

---
+++ battery/nubbin_battery.py
@@ -0,0 +1,7 @@
+from battery.battery import Battery
+
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
